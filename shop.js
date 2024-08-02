8490ce33b3a4d9a85533576965a4f262c0996e81
yazidibrahim <yazidibrahimanp@gmail.com>
2024-08-02 15:30:55 +0530

    first commit

---
+++ shop.js
@@ -0,0 +1,31 @@
+
+
+//   fetch("https://fakestoreapi.com/products")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+//   $.getJSON('https://fakestoreapi.com/products"', function(data) {
+
+//     var resources = data.resources.map(resource => `<a href="${resource.url}">${resource.description}</a><br>`),
+//         text = `<h2>${data.license_title}</h2><br>
+//                ${resources.join('')}`;
+
+//     $(".mypanel").html(text);
+//   });
+
+
+// $.getJSON( "https://fakestoreapi.com/products", function( data ) {
+//     var items = [];
+
+//     $.each( data, function( key, val ) {
+
+//       items.push( "<li id='" + key + "'>" + val + "</li>" );
+//       console.log(items)
+//     });
+   
+//     $( "<ul/>", {
+//       "class": "my-new-list",
+//       html: items.join( "" )
+//     }).appendTo( "body" );
+//   });
+
