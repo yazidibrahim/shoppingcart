45fc2c75d43a044139e4bf892b7b6c86add99cdc
yazidibrahim <83606566+yazidibrahim@users.noreply.github.com>
2024-08-05 01:33:22 +0530

    Add files via upload
    hi

--- shop.js
+++ shop.js
@@ -31,145 +31,110 @@
 
 $(document).ready(function(){
 
-$.ajax(
-    {
-        url:"https://fakestoreapi.com/products",
-        method:'get',
-        success:function(data){
-            // $.each(data, function(index,value) {
-            //     console.log(data)
-            //     // console.log('index: ' + index + '   ' + 'value: ' + value['title']);
-            //     // console.log('index: ' + index + '   ' + 'value: ' + value['price']);
-            //     // console.log('index: ' + index + '   ' + 'value: ' + value['description']);
-            //     // console.log('index: ' + index + '   ' + 'value: ' + value['image']);
-            //     let title = value['title'];
-            //     let price = value['price'];
+    $.ajax(
+        {
+            url:"https://fakestoreapi.com/products",
+            method:'get',
+            success:function(data){
+                // $.each(data, function(index,value) {
+                //     console.log(data)
+                //     // console.log('index: ' + index + '   ' + 'value: ' + value['title']);
+                //     // console.log('index: ' + index + '   ' + 'value: ' + value['price']);
+                //     // console.log('index: ' + index + '   ' + 'value: ' + value['description']);
+                //     // console.log('index: ' + index + '   ' + 'value: ' + value['image']);
+                //     let title = value['title'];
+                //     let price = value['price'];
+                    
+                //     $("#img1").html(`<img height='150px' width='150px' src="${value['image']}">`)
+                //     $(".play").off().click(function() {
+                //         let link = $(this).data('link');
+                //         $("#display").empty().append('<embed width="480" height="500" src="' + link + '">');
+                //     });
+                //     $('#t1').append(title[index]);
+                //     $('#price1').append(price);
+                    
+                //    entering datas into the card
+                console.log(data.length);
+                //    for (var i =0;i<data.id.length) 
                 
-            //     $("#img1").html(`<img height='150px' width='150px' src="${value['image']}">`)
-            //     $(".play").off().click(function() {
-            //         let link = $(this).data('link');
-            //         $("#display").empty().append('<embed width="480" height="500" src="' + link + '">');
-            //     });
-            //     $('#t1').append(title[index]);
-            //     $('#price1').append(price);
+                const qty = [];
                 
-            //    entering datas into the card
-            console.log(data.length);
-            //    for (var i =0;i<data.id.length) 
-            
-            const qty = [];
-
-            for(var i =1; i<data.length; i ++)
-            {
-                const user = data[i];
                 const id = [];
                 const title = [];
                 const image = [];
                 const price = [];
-                qty[i]=0;
-                console.log(id[i])
-                title[i] = user['title'];
-                console.log(title[i]);
-                image[i] = user['image'];
-                console.log(image[i]);
-                price[i] = user['price'];
-                console.log(price[i]);
-                $('#t'+i).append(title[i]);
-                $("#img"+i).html(`<img height='150px' width='150px' src="${image[i]}" >`);
-                $('#price'+i).append('$ '+price[i]);
+
+                for(var i =1; i<data.length; i ++)
+                {
+                    const user = data[i];
+                    qty[i]=0;
+                    console.log(id[i])
+                    title[i] = user['title'];
+                    console.log(title[i]);
+                    image[i] = user['image'];
+                    console.log(image[i]);
+                    price[i] = user['price'];
+                    console.log(price[i]);
+                    $('#t'+i).append(title[i]);
+                    $("#img"+i).html(`<img height='150px' width='150px' src="${image[i]}" >`);
+                    $('#price'+i).append('$ '+price[i]);
+                }   
+                   //first item is already added
                 
-
-
-
-
-
-
-
-
-            }   
-
-            $('#add1').click(function(){
-                qty[1]+=1;
-                $('#qty1').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[1]);
-                console.log("Quantity:"+qty); 
-                $("#imgc1").html(`<img height='150px' width='150px' src="${image[1]}" >`);
-                $('#pricec1').append('$ '+price[1]);
-                  
-            })
-            $('#add2').click(function(){
-                qty[2]+=1;
-                $('#qty2').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[2]);
-                console.log("Quantity:"+qty);   
-                $("#imgc2").html(`<img height='150px' width='150px' src="${image[2]}" >`);
-                $('#pricec2').append('$ '+price[2]);
-                  
-                
-            })
-            $('#add3').click(function(){
-                qty[3]+=1;
-                $('#qty3').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[3]);
-                console.log("Quantity:"+qty);   
-                $("#imgc3").html(`<img height='150px' width='150px' src="${image[3]}" >`);
-                $('#pricec3').append('$ '+price[3]);
-            })
-            $('#add4').click(function(){
-                qty[4]+=1;
-                $('#qty4').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[4]);
-                console.log("Quantity:"+qty);   
-                $("#imgc4").html(`<img height='150px' width='150px' src="${image[4]}" >`);
-                $('#pricec4').append('$ '+price[4]);
-                  
-                
-            })
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            //     console.log(data[1]);
-
-                //     // $('#t1').empty().append(user['title']);
-
-                //     // //  // Unbinding previous click events to avoid multiple bindings
-                //     // // $("product1").off().show(function() {
-                //     // //     $("#t1").empty().append("user['title']");
-                //     // // });
-                //     // $('#t2').empty().append(user['title']);
-                // title1 = user['title']
-                // $("#t1").append(title1);
-            
-
-
-
-
-
-                // }
-
-
-
-
-            // })
-
-        },
-        error:function(error){
-            console.log(error);
-        }
-    }
-)
-
-
-
-//inserting datas into the card
+                $('#add1').click(function(){
+                    qty[1]+=1;
+                    t = 0;
+                    $('#qty1').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[1]);
+                    console.log("Quantity:"+qty); 
     
-// $('#t1').append(title);
+                })   
+                $('#add2').click(function(){
+                    qty[2]+=1;
+                    $('#qty2').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[2]);
+                    console.log("Quantity:"+qty); 
+                })    
+                $('#add3').click(function(){
+                    
+                    qty[3]+=1;
+                    $('#qty3').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[3]);
+                    console.log("Quantity:"+qty); 
+                })    
+                $('#add4').click(function(){
+                    
+                    qty[4]+=1;
+                    $('#qty4').html('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16"><path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/></svg>'+qty[4]);
+                    console.log("Quantity:"+qty); 
+                })  
+                
 
+                
+                $('#calculate').click(function() {
+                    let totalPrice = 0;
+                    for (let i = 1; i < qty.length; i++) {
+                        totalPrice += qty[i] * price[i];
+                    }
+                    $('#total').text('$' + totalPrice.toFixed(2));
+                });
 
+                
 
-
-
-
-})
+    
+            },
+            error:function(error){
+                console.log(error);
+            }
+        }
+    )
+    
+    
+    
+    //inserting datas into the card
+        
+    // $('#t1').append(title);
+    
+    
+    
+    
+    
+    
+    })
